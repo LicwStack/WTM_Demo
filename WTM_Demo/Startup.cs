a9a858eb0fa1b178a310885bb9409287df223729
lichunwei <LicwStack@163.com>
2020-08-06 14:45:46 +0800

    删除未使用的引用

--- WTM_Demo/Startup.cs
+++ WTM_Demo/Startup.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using WalkingTec.Mvvm.TagHelpers.LayUI;
+using Microsoft.OpenApi.Models;
 using WalkingTec.Mvvm.Core;
 using WalkingTec.Mvvm.Mvc;
-using Microsoft.OpenApi.Models;
-using Microsoft.AspNetCore.SpaServices.Webpack;
+using WalkingTec.Mvvm.TagHelpers.LayUI;
 
 namespace WTM_Demo
 {
